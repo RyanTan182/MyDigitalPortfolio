c071adc50db7def9ac58abc9b592d0a645abe6ec
Ryan Tan <193190N@mymail.nyp.edu.sg>
2022-02-28 15:40:20 +0800

    first commit

---
+++ routes/main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+	const title = "Hi, I'm Ryan Tan!";
+	res.render('index', {title: title}) 
+});
+
+router.get('/about', (req, res) => {
+	const title = 'RYAN TAN';
+	res.render('about_me/about', {title: title}) 
+});
+
+
+router.get('/works', (req, res) => {
+	const title = 'RYAN TAN';
+	res.render('works/works', {title: title}) 
+});
+
+module.exports = router;
