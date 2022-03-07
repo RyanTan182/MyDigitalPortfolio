ac056fa9e2c469dea698e146bfcc49cf5555233a
Ryan Tan <193190N@mymail.nyp.edu.sg>
2022-03-07 20:56:54 +0800

    created first part of about me page

--- routes/main.js
+++ routes/main.js
@@ -3,19 +3,19 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-	const title = "Hi, I'm Ryan Tan!";
+	const title = "Ryan Tan";
 	res.render('home', {title: title}) 
 });
 
 router.get('/about', (req, res) => {
-	const title = 'RYAN TAN';
-	res.render('about_me/about', {title: title}) 
+	const title = 'About Me | Ryan Tan';
+	res.render('about_me/about', {title: "About Me | Ryan Tan"}) 
 });
 
 
 router.get('/works', (req, res) => {
-	const title = 'RYAN TAN';
-	res.render('works/works', {title: title}) 
+	const title = 'My Works | Ryan Tan';
+	res.render('works/works', {title: "My Works | Ryan Tan"}) 
 });
 
 module.exports = router;
