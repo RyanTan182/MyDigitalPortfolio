1b3f3d520e42f7763dbf263b03e03074055b9316
Ryan Tan <193190N@mymail.nyp.edu.sg>
2022-03-25 01:10:54 +0800

    working on third and fourth modal

--- public/js/works.js
+++ public/js/works.js
@@ -22,6 +22,9 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
 }
 
 
